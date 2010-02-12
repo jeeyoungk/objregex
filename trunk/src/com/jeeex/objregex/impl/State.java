f7eaf2d58a1d486ee911183d278f1e5bc978aa97
jeeyoungk@gmail.com <jeeyoungk@gmail.com@6c1178f2-12d8-11df-a1e4-8def113ae3e8>
2010-02-12 05:45:35 +0000

    Added more comments to the State interface.

--- trunk/src/com/jeeex/objregex/impl/State.java
+++ trunk/src/com/jeeex/objregex/impl/State.java
@@ -6,17 +6,28 @@ import com.google.common.collect.Multimap;
  * A class representing a group of NFA states.
  * <p>
  * Each {@link State} is either a single state, or a group of states.
- * {@link #getHead() Head} and {@link Tail} represent the entry and exit point
- * in this group of state.
+ * {@link State} objects are special in a sense that, they have exactly one each
+ * {@link #getHead() Head} and {@link #getTail() tail} states, which are the
+ * states with transitions to / from other states that are not part of this
+ * {@link State}. It is possible that the {@code head} and {@code tail} refer to
+ * the same state.
  * 
  * @author jeekim
- * 
+ * @since 2010-02-11
  */
 public interface State {
+
 	public State getHead();
 
 	public State getTail();
 
+	/**
+	 * Retrieves all the {@link State}s that are reachable from this
+	 * {@link State}. Note {@code a.getTransitions()} is equivalent to {@code
+	 * a.getHead().getTransitions()}.
+	 * 
+	 * @return
+	 */
 	public Multimap<TransitionIdentifier, State> getTransitions();
 
 	/**
