ac9f749fa77896abd2926e7a08cf330e7371463b
jeeyoungk@gmail.com <jeeyoungk@gmail.com@6c1178f2-12d8-11df-a1e4-8def113ae3e8>
2010-02-13 04:33:24 +0000

    Fixed getNodeList, added more methods.

--- trunk/src/com/jeeex/objregex/javacc/EnhancedNode.java
+++ trunk/src/com/jeeex/objregex/javacc/EnhancedNode.java
@@ -1,10 +1,11 @@
 package com.jeeex.objregex.javacc;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 
 /**
  * A wrapper around {@link SimpleNode} to provide more modern API.
@@ -20,13 +21,50 @@ public class EnhancedNode extends SimpleNode {
 	}
 
 	/**
-	 * Returns the unmodifiable list, containing all the children of this node.
+	 * Returns the unmodifiable list, containing all the children nodes of this
+	 * node.
 	 */
 	public List<EnhancedNode> getNodeList() {
 		if (children == null) {
 			return ImmutableList.of();
 		}
-		return Collections.unmodifiableList(Arrays
-				.asList((EnhancedNode[]) children));
+		EnhancedNode[] echildren = new EnhancedNode[children.length];
+		for (int i = 0; i < children.length; i++) {
+			echildren[i] = (EnhancedNode) children[i];
+		}
+		return Arrays.asList(echildren);
+	}
+
+	/**
+	 * Returns the unmodifiable list, containing all the children nodes of this
+	 * node, starting from {@code index}.
+	 */
+	public List<EnhancedNode> getNodeList(int index) {
+		return getNodeList().subList(index, children.length);
+	}
+
+	/**
+	 * Convenient way to get the first child of the node.
+	 * 
+	 * @return
+	 */
+	public EnhancedNode getFirstChild() {
+		return (EnhancedNode) children[0];
+	}
+
+	/**
+	 * Returns the list of tokens in this Node.
+	 * 
+	 * @return
+	 */
+	public List<Token> getTokenList() {
+		Builder<Token> builder = ImmutableList.builder();
+		Token t = jjtGetFirstToken();
+		do {
+			builder.add(t);
+			t = t.next;
+		} while (t != jjtGetLastToken());
+
+		return builder.build();
 	}
 }
