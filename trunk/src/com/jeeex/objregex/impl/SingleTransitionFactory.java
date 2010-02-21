6be0a0f015154e905f85b1727400b90e363b90be
jeeyoungk@gmail.com <jeeyoungk@gmail.com@6c1178f2-12d8-11df-a1e4-8def113ae3e8>
2010-02-21 18:23:12 +0000

    Abstraced away single transition state creation from SingleLazyStateManager to SingleTransitionFactory.

---
+++ trunk/src/com/jeeex/objregex/impl/SingleTransitionFactory.java
@@ -0,0 +1,19 @@
+package com.jeeex.objregex.impl;
+
+/**
+ * Interface that defines a method that creates a single transition NFA
+ * {@link State} for the given {@link TransitionIdentifier}.
+ * <p>
+ * 
+ * @author Jeeyoung Kim
+ * @since 2010-02-21
+ * 
+ */
+public interface SingleTransitionFactory {
+
+	/**
+	 * Create a single transition.
+	 */
+	public State singleTransition(TransitionIdentifier tid)
+			throws NullPointerException;
+}
